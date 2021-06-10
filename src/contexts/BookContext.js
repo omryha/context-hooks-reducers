3a6f905cb3b8007ca55afacc294d92042a0cfa5b
hezy laplacian <hezylaplacian@gmail.com>
2021-06-10 09:30:31 +0300

    Initial commit

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,10 +1,20 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(BookReducer, []);
+  const [books, dispatch] = useReducer(BookReducer, [], () => {
+    // Get books from Local Storage when initializing
+    return localStorage.getItem('books')
+      ? JSON.parse(localStorage.getItem('books'))
+      : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+    // sessionStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
